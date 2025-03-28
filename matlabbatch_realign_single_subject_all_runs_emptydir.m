0cb0dea1c8666d30d3378dd9f07a63830f8721f6
jishnasun <144394163+jishnasun@users.noreply.github.com>
2025-03-28 11:21:36 -0400

    Explanatory comments added

--- matlabbatch_realign_single_subject_all_runs_emptydir.m
+++ matlabbatch_realign_single_subject_all_runs_emptydir.m
@@ -3,7 +3,7 @@
 % spm SPM - Unknown
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_cd.dir = {'/home/jovyan/data/derivatives/ya'};
+matlabbatch{1}.cfg_basicio.file_dir.dir_ops.cfg_cd.dir = {'/home/jovyan/data/derivatives'};  % Change this directory to the directory that holds all the subject files
 matlabbatch{2}.cfg_basicio.file_dir.dir_ops.cfg_named_dir.name = 'Subject Folder';
 matlabbatch{2}.cfg_basicio.file_dir.dir_ops.cfg_named_dir.dirs = {'<UNDEFINED>'};
 matlabbatch{3}.cfg_basicio.file_dir.file_ops.file_fplist.dir(1) = cfg_dep('Named Directory Selector: Subject Folder(1)', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','dirs', '{}',{1}));
@@ -19,7 +19,7 @@ matlabbatch{4}.cfg_basicio.file_dir.file_ops.cfg_file_split.index = {
                                                                      5
                                                                      6
                                                                      7
-                                                                     }';
+                                                                     }'; % Change number in index and below depending on the number of runs in each subject file (in this case there are 7 runs per participant)
 matlabbatch{5}.spm.spatial.realign.estimate.data{1}(1) = cfg_dep('File Set Split: fMRI Runs (1)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{1}));
 matlabbatch{5}.spm.spatial.realign.estimate.data{2}(1) = cfg_dep('File Set Split: fMRI Runs (2)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{2}));
 matlabbatch{5}.spm.spatial.realign.estimate.data{3}(1) = cfg_dep('File Set Split: fMRI Runs (3)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{3}));
@@ -27,10 +27,10 @@ matlabbatch{5}.spm.spatial.realign.estimate.data{4}(1) = cfg_dep('File Set Split
 matlabbatch{5}.spm.spatial.realign.estimate.data{5}(1) = cfg_dep('File Set Split: fMRI Runs (5)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{5}));
 matlabbatch{5}.spm.spatial.realign.estimate.data{6}(1) = cfg_dep('File Set Split: fMRI Runs (6)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{6}));
 matlabbatch{5}.spm.spatial.realign.estimate.data{7}(1) = cfg_dep('File Set Split: fMRI Runs (7)', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('{}',{7}));
-matlabbatch{5}.spm.spatial.realign.estimate.eoptions.quality = 0.9;
+matlabbatch{5}.spm.spatial.realign.estimate.eoptions.quality = 0.9; % can also change quality to increase speed, but values will be less accurate
 matlabbatch{5}.spm.spatial.realign.estimate.eoptions.sep = 4;
 matlabbatch{5}.spm.spatial.realign.estimate.eoptions.fwhm = 5;
-matlabbatch{5}.spm.spatial.realign.estimate.eoptions.rtm = 0;
+matlabbatch{5}.spm.spatial.realign.estimate.eoptions.rtm = 0; % Aligns to first rather than the mean, for the sake of speed (change to 1 to align to mean)
 matlabbatch{5}.spm.spatial.realign.estimate.eoptions.interp = 2;
 matlabbatch{5}.spm.spatial.realign.estimate.eoptions.wrap = [0 0 0];
 matlabbatch{5}.spm.spatial.realign.estimate.eoptions.weight = '';
