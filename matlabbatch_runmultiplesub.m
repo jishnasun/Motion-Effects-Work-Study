432b6b8c0396cfd7ca37263dc07c9340eeddf0c6
jishnasun <144394163+jishnasun@users.noreply.github.com>
2025-03-28 11:17:21 -0400

    SPM batch for running multiple batch jobs
    This is the code that uses the matlabbatch_realign_single_subject_all_runs_emptydir.m and runs it over all inputted directories

---
+++ matlabbatch_runmultiplesub.m
@@ -0,0 +1,12 @@
+%-----------------------------------------------------------------------
+% Job saved on 27-Mar-2025 15:12:38 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - Unknown
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.cfg_basicio.run_ops.runjobs.jobs = {'/neurodesktop-storage/python files/matlabbatch_realign_single_subject_all_runs_emptydir.m'};
+matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{1}{1}.indir = {'/home/jovyan/data/derivatives/avp/sub-1435220200204'};
+matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{2}{1}.indir = {'/data/derivatives/avp/sub-1469120221102'};
+matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{3}{1}.indir = {'/data/derivatives/avp/sub-15273'};
+matlabbatch{1}.cfg_basicio.run_ops.runjobs.inputs{4}{1}.indir = {'/data/derivatives/avp/sub-858020220602'};
+matlabbatch{1}.cfg_basicio.run_ops.runjobs.save.dontsave = false;
+matlabbatch{1}.cfg_basicio.run_ops.runjobs.missing = 'skip';
