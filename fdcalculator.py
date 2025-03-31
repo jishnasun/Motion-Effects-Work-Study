6638c1c318e435e33f48a4a03a96e792ae71a4be
jishnasun <144394163+jishnasun@users.noreply.github.com>
2025-03-31 12:37:35 -0400

    Explanatory Comments added
    Cleaned up code and included what the output in the txt file will look like

--- fdcalculator.py
+++ fdcalculator.py
@@ -13,15 +13,18 @@ f = open(file_name, "w")
 # In the case that a txt file exists with this name, its contents will be erased and replaced with the new data
 f.close()
 f = open(file_name, "a")  #then reopen to begin appending data
-#f.write("Patient Id\tMean FD 1\tMax FD 1\tMean FD 2\tMax FD 2\tMean FD 3\tMax FD 3\tMean FD 4\tMax FD 4\tMean FD 5\tMax FD 5\tMean FD 6\tMax FD 6\tMean FD 7\tMax FD 7\n")  # Label the columns
+
+#Output will have the following form per row:
+# Patient Id, Mean FD 1, Max FD 1, Mean FD 2, Max FD 2, Mean FD 3, Max FD 3, Mean FD 4, Max FD 4, Mean FD 5, Max FD 5, Mean FD 6, Max FD 6, Mean FD 7, Max FD 7
+# If the subject has less than 7 runs it will simply leave the extra columns blank
 
 # Using standard head radius
 radius = 50
 
 # Define directory with subject files
-parent_dir = "/home/jovyan/data/derivatives/ya"  # Change last directory for each section
+parent_dir = "/home/jovyan/data/derivatives"  # Parent directory; should containt NIFTI subject files in standard BIDSCOIN format
 
-# Loop through each subdirectory in the parent directory
+# Loop through each subject file in the parent directory
 for subdir in sorted(os.listdir(parent_dir)):  # Sort for consistency
     subdir_path = os.path.join(parent_dir, subdir)
     
@@ -31,7 +34,7 @@ for subdir in sorted(os.listdir(parent_dir)):  # Sort for consistency
         print(f"\nProcessing directory: {subject_name}")
         f.write(f"{subject_name}\t")
 
-        # Find all realignment txt files starting with "rp" in this subject file
+        # Find all realignment txt files starting with "rp" in this subject file -- searches in 'func' directory within subject file
         txt_files = glob.glob(os.path.join(subdir_path, "func", "rp*.txt"))
 
         if not txt_files:
@@ -53,10 +56,10 @@ for subdir in sorted(os.listdir(parent_dir)):  # Sort for consistency
             mean_fd = np.mean(fd)
             max_fd = np.max(fd)
 
-            # Annotate FD results for the run
+            # Annotate FD results for the run (only includes three decimal places)
             f.write(f"{mean_fd:.3f}\t{max_fd:.3f}\t")
             
-        f.write("\n")
+        f.write("\n") # new row, new subject
 
 
 f.close() # close FD data file
